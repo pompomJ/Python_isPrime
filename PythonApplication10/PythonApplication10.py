a781f0b4287357777525d4a1bc50cd2d2e90ec98
pompomJ <67637461+pompomJ@users.noreply.github.com>
2024-09-26 19:47:59 +0900

    Update PythonApplication10.py
    3行目でTypeErrorが出たのでi明示的にint型にキャストしました。

--- PythonApplication10/PythonApplication10.py
+++ PythonApplication10/PythonApplication10.py
@@ -1,8 +1,9 @@
 def isprime(n):
     prime = True
-    for i in range(2, n/2):
+    for i in range(2, int(n/2)):
         if n % i == 0:
             prime = False
             break
         return prime
 
+isPrime(17)
